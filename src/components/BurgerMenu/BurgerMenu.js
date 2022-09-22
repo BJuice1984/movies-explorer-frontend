af290eef4eb0821f11c675c13473f7c530123a2a
BJuice <beetle_juice84@mail.ru>
2022-09-23 02:05:05 +0300

    refr/добавить css для стилизации компонента

--- src/components/BurgerMenu/BurgerMenu.js
+++ src/components/BurgerMenu/BurgerMenu.js
@@ -4,20 +4,20 @@ import './BurgerMenu.css';
 function BurgerMenu() {
 
   return (
-    <body>
-    <input type="checkbox" className="toggler"></input>
-    <div className="hamburger"><div></div></div>
-    <div className="menu">
-        <div>
-            <ul>
-                <li><a href='#'>Home</a></li>
-                <li><a href='#'>About</a></li>
-                <li><a href='#'>Services</a></li>
-                <li><a href='#'>Contact</a></li>
-            </ul>
-        </div>
-    </div>
-</body>
+    <section className="burger-menu">
+      <input type="checkbox" className="toggler"></input>
+      <div className="hamburger"><div></div></div>
+      {/* <div className="menu">
+          <div>
+              <ul>
+                  <li><a href='#'>Home</a></li>
+                  <li><a href='#'>About</a></li>
+                  <li><a href='#'>Services</a></li>
+                  <li><a href='#'>Contact</a></li>
+              </ul>
+          </div>
+      </div> */}
+    </section>
   )
 
 }
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -10,6 +10,41 @@ function Profile(props) {
     <section className="profile">
       <Logo />
       <BurgerMenu />
+      <h2 className="profile__title">Привет, Ил&#33;</h2>
+      <form className="profile__input-form" >
+        <label className="profile__input-form-label">
+          <span className="profile__input-name">Имя</span>
+          <input
+          placeholder="Trf"
+          value={`Катя`}
+
+
+          className="profile__input-text"
+          type="text"
+          name="name"
+          id="name"
+          required
+          minLength="2"
+          maxLength="20" />
+        </label>
+        <label className="profile__input-form-label">
+          <span className="profile__input-name">Email</span>
+          <input
+          placeholder="@@@"
+          value={`Катя@`}
+
+
+          className="profile__input-text" 
+          type="email"
+          name="email"
+          id="email"
+          required
+          minLength="6"
+          maxLength="20" />
+        </label>
+        <button className="profile__button" type="submit">Редактировать</button>
+        <Link className="profile__link" to="/sign-in">Выйти из аккаунта</Link>
+      </form>
     </section>
   )
 }
