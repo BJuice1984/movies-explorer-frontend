4762e6482d9fb35f1505f13fe61cbc07b3ca0ab5
BJuice <beetle_juice84@mail.ru>
2022-10-10 18:05:36 +0300

    refr/Редактировать CSS BurgerMenu.js

--- src/components/BurgerMenu/BurgerMenu.js
+++ src/components/BurgerMenu/BurgerMenu.js
@@ -1,23 +1,24 @@
 import React from "react";
 import './BurgerMenu.css';
 
-function BurgerMenu() {
+function BurgerMenu({ onMainPage }) {
 
   return (
-    <section className="burger-menu">
-      <input type="checkbox" className="toggler"></input>
-      <div className="hamburger"><div></div></div>
-      {/* <div className="menu">
-          <div>
-              <ul>
-                  <li><a href='#'>Home</a></li>
-                  <li><a href='#'>About</a></li>
-                  <li><a href='#'>Services</a></li>
-                  <li><a href='#'>Contact</a></li>
+    <div className={`burger-menu ${onMainPage ? 'burger-menu_type__visible' : ''}`}>
+      <input type="checkbox" className="burger-menu__toggler"></input>
+      <div className="hamburger">
+        <div className="hamburger__btn"></div>
+      </div>
+      <div className="menu">
+
+              <ul className="menu__list">
+                  <li><a href='#'>Главная</a></li>
+                  <li><a href='#'>Фильмы</a></li>
+                  <li><a href='#'>Сохраненные фильмы</a></li>
               </ul>
-          </div>
-      </div> */}
-    </section>
+
+      </div>
+    </div>
   )
 
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Logo from "../Logo/Logo";
+import BurgerMenu from "../BurgerMenu/BurgerMenu";
 import { Link } from 'react-router-dom';
 import './Header.css';
 
@@ -7,6 +8,7 @@ function Header({ isMainPage }) {
 
   return (
     <section className={`header ${isMainPage ? 'header_type_main' : ''}`}>
+      <BurgerMenu onMainPage={!isMainPage}/>
       <div className="header__container">
         <Logo />
         <Link className={`header__reg-button ${isMainPage ? 'header__reg-button_type_visible' : ''}`} to="/sign-up">Регистрация</Link>
