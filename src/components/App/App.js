690c4d0975a4a9b625326ca85a18cb1b53750f6d
BJuice <beetle_juice84@mail.ru>
2022-10-13 22:52:17 +0300

    refr/Поправить стили в Login.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Register from '../Register/Register';
 import Login from '../Login/Login';
@@ -9,14 +8,9 @@ import SavedMovies from '../SavedMovies/SavedMovies';
 import PageNotFound from '../PageNotFound/PageNotFound';
 import { Routes, Route } from 'react-router-dom';
 import { CurrentUserContext } from '../../context/CurrnetUserContext';
-// import { Routes, Route, useNavigate, Redirect } from 'react-router-dom';
 import './App.css';
 
 import useLogin from '../../hooks/useLogin';
-
-
-
-// import * as Auth from '../../utils/MainApi';
 import { moviesApi } from '../../utils/MoviesApi';
 
 
@@ -31,37 +25,7 @@ function App() {
     handleLogin
   } = useLogin();
 
-  console.log(currentUser)
-
-  // const navigate = useNavigate();
-
-  // // const [currentUser, setCurrentUser] = React.useState({});
-  // const [loggedIn, setLoggedIn] = React.useState(true);
-  // // const [savedFilms, setSavedFilms] = React.useState([]);
-
-
-  // const handleRegister = ({ name, password, email }) => {
-  //   return Auth.register(name, password, email)
-  //   .then(() => {
-  //     navigate('/sign-in')
-  //   })
-  //   .catch(() => {
-  //     navigate('/sign-up')
-  //   })
-  // }
-
-  // const handleLogin = ({ password, email }) => {
-  //   return Auth.authorize(password, email)
-  //   .then((data) => {
-  //     if (data) {
-
-  //       setLoggedIn(true);
-  //     }
-  //   })
-  //   .catch(() => {
-  //     navigate('/sign-in')
-  //   })
-  // }
+  console.log(currentUser, loggedIn)
 
   React.useEffect(() => {
     moviesApi.getInitialMovies()
