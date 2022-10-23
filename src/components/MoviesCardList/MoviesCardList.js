43109ec7be72644fa4b1f443126f96f15ff90cf2
BJuice <beetle_juice84@mail.ru>
2022-10-23 04:15:35 +0300

    feat/Добавить состояние disable для кнопки Ещё в MoviesCardList.js

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,15 +1,29 @@
 import React from "react";
+import { useLocation } from 'react-router-dom';
 import './MoviesCardList.css';
 import MoviesCard from "../MoviesCard/MoviesCard";
 
 function MoviesCardList({ movies, handleAddUserMovie }) {
+  const [isMoviesPage, setIsMoviesPage] = React.useState(true);
+  const location = useLocation();
+
+  React.useEffect(() => {
+    if (location.pathname === '/saved-movies') {
+      setIsMoviesPage(false);
+    } else {
+      setIsMoviesPage(true);
+    }
+  }, [location]);
 
   return(
     <section className="movie-card-list">
       <ul className="movie-card-list__items">
         {movies.map(movie => <MoviesCard movie={movie} key={movie.id ? movie.id : movie.movieId} handleAddUserMovie={handleAddUserMovie}/>)}
       </ul>
-      <button className="movie-card-list__btn">Ещё</button>
+      <button
+        className={`movie-card-list__btn ${isMoviesPage ? '' : 'movie-card-list__btn_type_disable'}`}
+        type="button" 
+        aria-label="Показать ещё">Ещё</button>
     </section>
   )
 }
